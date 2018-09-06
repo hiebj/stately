f6df3ed652df7a4f21673a9e165c60ff89c80d54
Jon Hieb <jon.hieb@logrhythm.com>
2018-09-06 17:27:05 -0600

    add 'pause' effect to release.sh

--- release.sh
+++ release.sh
@@ -3,7 +3,10 @@ set -e
 rm -rf node_modules
 npm install
 npm test
-npm version $@
+newversion=$(npm version $@)
 npm run build
+blue=$'\033[0;36m'
+noColor=$'\033[0m'
+read -rsp "About to publish ${blue}${npm_package_name} ${newversion}${noColor}"$'.\nPress Enter to continue or Ctrl+C to cancel.'
 cd dist
 npm publish
